baf0cc263d71cd8f9a3e405084b8353d6736414f
Omar Molina <A01423485@itesm.mx>
2020-11-19 18:02:32 -0600

    Laboratorio 20

---
+++ Lab20/preguntas.php
@@ -0,0 +1,33 @@
+<?php
+
+
+    include("_header.html");
+  ?>  
+    <div class="container">
+    <p>Preguntas</p>
+        
+        
+    ¿Qué alternativas a jQuery existen?
+    <br>
+    <strong>XUI. La particularidad de XUI se ve enfocada en que funciona para la creación y el desarrollo de las aplicaciones para teléfonos inteligentes.</strong>    
+    <br>
+    ¿Qué es una promesa en js?
+    <br>
+    <strong>Una Promise (promesa en castellano) es un objeto que representa la terminación o el fracaso de una operación asíncrona.</strong>
+    <br>
+    ¿Cómo funcionan async y await?
+    <strong>La declaración de función async define una función asíncrona, la cual devuelve un objeto AsyncFunction. 
+            Y await sirve para que la ejecución de una función async sea pausada hasta que una Promise sea terminada o rechazada.
+    <br>
+    </strong>
+    <br>
+    <p>REFERENCIAS</p>
+    
+   <p>https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Sentencias/funcion_asincrona</p> 
+    
+    </div>
+<?php
+
+    include("_footer.html");
+
+?>
